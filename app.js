cd7dbd556cf2b93f1fb744d46f3db95cdfe17f7b
vldbnt <l.valdebenito@hotmail.com>
2025-03-12 11:05:28 -0500

    update agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+    if(!nombreAmigo) {
+        alert("Debes ingresar un nombre");
+        return;
+    }
+    amigos.push(nombreAmigo);
+    inputAmigo.value = "";
+    console.log(amigos);
+    
+
+}
