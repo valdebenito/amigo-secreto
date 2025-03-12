0c8d759a4260a3bb025f543d6d50cc021c95a28b
vldbnt <l.valdebenito@hotmail.com>
2025-03-12 12:08:16 -0500

    update funciones para agregar amigos, sorteo y presentacion en el index

--- app.js
+++ app.js
@@ -10,7 +10,44 @@ function agregarAmigo() {
     }
     amigos.push(nombreAmigo);
     inputAmigo.value = "";
-    console.log(amigos);
-    
+    inputAmigo.focus();
+    //console.log(amigos);  
+    renderizarAmigos();  
+}
 
+function renderizarAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    for(let i=0; i<amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+function sortearAmigos() {
+    if(amigos.length===0){
+        alert("No hay amigos para sortear");
+        return;
+    }
+    if(amigos.length < 2) {
+        alert("Debes ingresar al menos dos amigos");
+        return;
+    }
+    let amigosSorteados = amigos.slice();
+    for(let i=0; i<amigos.length; i++) {
+        let amigo = amigosSorteados[i];
+        let amigoSorteado = amigosSorteados[Math.floor(Math.random() * amigosSorteados.length)];
+        amigosSorteados[i] = amigoSorteado;
+        amigosSorteados[amigosSorteados.indexOf(amigoSorteado)] = amigo;
+    }
+    renderizarSorteo(amigos, amigosSorteados);
+}
+function renderizarSorteo(amigos, amigosSorteados) {
+    let listaSorteo = document.getElementById("resultado");
+    listaSorteo.innerHTML = "";
+    for(let i=0; i<amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i] + " le regala a " + amigosSorteados[i];
+        listaSorteo.appendChild(li);
+    }
 }
